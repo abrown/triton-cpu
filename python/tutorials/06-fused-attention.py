668da10eaacb83428b15947b63615a957d50b1b5
Andrew Brown <andrew@kernelize.ai>
2026-03-05 08:32:42 -0800

    Avoid FP8 on CPU

--- python/tutorials/06-fused-attention.py
+++ python/tutorials/06-fused-attention.py
@@ -628,7 +628,7 @@ class _attention(torch.autograd.Function):
 
 attention = _attention.apply
 
-TORCH_HAS_FP8 = hasattr(torch, 'float8_e5m2')
+TORCH_HAS_FP8 = hasattr(torch, 'float8_e5m2') and not is_cpu()
 
 
 @pytest.mark.parametrize("Z", [1, 4])
@@ -705,7 +705,7 @@ try:
 except BaseException:
     HAS_FLASH = False
 
-TORCH_HAS_FP8 = hasattr(torch, 'float8_e5m2')
+TORCH_HAS_FP8 = hasattr(torch, 'float8_e5m2') and not is_cpu()
 BATCH, N_HEADS = 4, 32
 # vary seq length for fixed head and batch=4
 configs = []
