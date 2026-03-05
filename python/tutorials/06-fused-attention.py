e870292d128b89b027c262e15247507f07d1af76
Andrew Brown <andrew@kernelize.ai>
2026-03-05 14:02:22 -0800

    Add `is_cpu()` check

--- python/tutorials/06-fused-attention.py
+++ python/tutorials/06-fused-attention.py
@@ -32,6 +32,10 @@ def is_cuda():
     return triton.runtime.driver.active.get_current_target().backend == "cuda"
 
 
+def is_cpu():
+    return triton.runtime.driver.active.get_current_target().backend == "cpu"
+
+
 def supports_host_descriptor():
     return is_cuda() and torch.cuda.get_device_capability()[0] >= 9
 
